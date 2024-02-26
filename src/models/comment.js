83e47e118d8df18439bb88557d1711510d0fe51c
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-27 01:53:38 +0530

    Updated comment model with refPath

--- src/models/comment.js
+++ src/models/comment.js
@@ -7,9 +7,22 @@ const commentSchema = new mongoose.Schema({
         type: String,
         require: true,
     },
-    userEmail: {
-        type: String
+    userId: {
+        ype: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'  //we are using refPath
     }
+
 }, { timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema); //This model name i.e. "Tweet" will become "Tweets" . Its default behaviour of ODMs and ORMs 
