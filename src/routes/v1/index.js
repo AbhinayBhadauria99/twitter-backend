0a8202c9f822127b20fbc91f90c8f2d9f471697c
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-27 01:33:49 +0530

    Added Toggling likes feature as a API i.e. tested via Postman

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import { createTweet } from '../../controllers/tweet-controller.js';
 import { toggleLike } from '../../controllers/like-controller.js';
 
+
+
 const router = express.Router();
 
 router.post('/tweets', createTweet);
